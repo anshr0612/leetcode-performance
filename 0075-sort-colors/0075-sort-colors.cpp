a2edee1bd7fda7b16386f009f83e0144037a5aa4
anshr0612 <anshr0612@gmail.com>
2026-07-25 20:18:44 +0530

    Optimize sortColors with one-pass algorithm
    Refactored sortColors function to use a single pass sorting algorithm.

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,14 +2,43 @@ class Solution {
 public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
+        /*Time complexity O(n), space complexity O(1).
+        But this contains 2 passes.
+
+        int a=0,b=0,c=0;
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(nums[i]<nums[j]){
-                    int temp=nums[i];
-                    nums[i]=nums[j];
-                    nums[j]=temp;
-                }
-            }
+            if(nums[i]==0)a++;
+            else if(nums[i]==1)b++;
+            else c++;
+        }
+        for(int i=0;i<n;i++){
+        while(a>0){
+            nums[i++]=0;
+            a--;
+        }
+        while(b>0){
+            nums[i++]=1;
+            b--;
+        }
+        while(c>0){
+            nums[i++]=2;
+            c--;
+        }
+        }
+        */
+        // This contains 1 pass.
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+        if(nums[mid]==0){
+            swap(nums[mid],nums[low]);
+            low++;
+            mid++;
+        }else if(nums[mid]==1){
+            mid++;
+        }else if(nums[mid]==2){
+            swap(nums[mid],nums[high]);
+            high--;
+        }
         }
     }
 };
